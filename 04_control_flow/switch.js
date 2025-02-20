3cc390fd2a86071429ca85a9f835a03a123a175d
1vikram <vikramkataria098@gmail.com>
2025-02-20 22:51:00 +0530

    switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+// syntax of switch statment
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/        
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("fabuary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+
+    default: console.log("give number is not for month");
+        break;
+}
