525a8c41dca742a8c771017d30a3ad6309684752
1vikram <vikramkataria098@gmail.com>
2025-02-20 16:04:38 +0530

    try by me

---
+++ 03_basics_JavaScript/05_test.js
@@ -0,0 +1,14 @@
+((userName)=>{
+    console.log(`the name of the user is ${userName}`);    
+})("vikram");
+
+const car = {
+    car1: "BMW",
+    car2: "belleno",
+    car3: "fronx",
+    car4: "RR"
+};
+
+((carName)=>{
+    console.log(`the car1 is ${carName.car1}, the secone one is ${carName.car2}`);
+})(car)
